f8798977022f6249d80068fc38691affe42ee6a2
evantianx <evantianx@gmail.com>
2020-01-29 20:22:43 +0800

    chore: delete console

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -4,6 +4,5 @@ import { useTodosStore } from "../store";
 
 export const VisibleTodoList = () => {
   const { todos, toggleTodo } = useTodosStore();
-  console.log(todos);
   return <TodoList todos={todos} toggleTodo={toggleTodo} />;
 };
